b6a425663a647946cfe9f0eb503bfe77301e6f63
Plat <github@p1at.dev>
2022-10-04 17:24:22 +0900

    feat: deps

---
+++ deps.ts
@@ -0,0 +1,13 @@
+// serve
+export * from "https://deno.land/std@0.158.0/http/server.ts"
+export * from "https://deno.land/std@0.158.0/http/http_status.ts"
+
+// whisper
+export * from "https://deno.land/x/whisper@0.1.2/mod.ts"
+
+// hono
+export * as h from "https://deno.land/x/hono@v2.2.5/mod.ts"
+export * as hono from "https://deno.land/x/hono@v2.2.5/hono.ts"
+
+// test
+export * from "https://deno.land/std@0.158.0/testing/asserts.ts"
